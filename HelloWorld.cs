f7a8f7f67075f7056488068b9b28001ccd9cdc6f
zelsiz <95526153+zelsiz@users.noreply.github.com>
2023-06-09 15:22:53 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //эээ, ну типо доброе утро, ой, то есть привет мир
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
